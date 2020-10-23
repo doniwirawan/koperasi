a1fb38d7140bff9034c92323d40660bd9a0226d3
doniwirawan <doniwirawan166@gmail.com>
2020-10-23 21:11:41 +0800

    adding a whole file

---
+++ script.js
@@ -0,0 +1,13 @@
+// function bodyBlur(){
+// 	$('.content-non-blur').css("filter","blur(20px)");
+// }
+// function backNormal(){
+// 	$('.content-non-blur').css("filter","none");
+
+// }
+$('.show-menu').click(function(){
+	$('.hidden-menu').show(200);
+})
+$('.close-button').click(function(){
+	$('.hidden-menu').hide(200);
+})
